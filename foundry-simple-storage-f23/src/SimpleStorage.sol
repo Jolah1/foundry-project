2a70d3d42a615644cd7d39f4d244edbf0678b8b6
jolade okunlade <jokunlade@gmail.com>
2024-10-05 00:26:32 +0100

    chore: forge init

---
+++ foundry-simple-storage-f23/src/SimpleStorage.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27; // version
+
+contract SimpleStorage {
+    // favoriteNumber gets initialized to 0 if no value is given.
+    uint256 myFavoriteNumber; // 0
+
+    // uint256[] listOfFavoriteNumbers;
+    struct person {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // dynamic array
+    person[] public listOfPeople; // []
+
+    //may 232
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        myFavoriteNumber = _favoriteNumber; // +5
+    }
+
+    // view, pure
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
+
+    // calldata, memory, storage (temporary variables)
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
